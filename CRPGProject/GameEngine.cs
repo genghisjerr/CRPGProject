0ca96df06c3503a270acdda41990a60f80f2c9a8
MUKILTEO\1630410 <1630410@mukilteo.wednet.edu>
2023-01-24 09:25:48 -0800

    Add project files.

---
+++ CRPGProject/GameEngine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRPGProject
+{
+    public static class GameEngine
+    {
+        public static string Version = "0.0.1";
+
+        public static void Initialize()
+        {
+            Console.WriteLine("Initializing Game Engine Version {0}", Version);
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ CRPGProject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//jerr '23
+namespace CRPGProject
+{
+    class Program
+    {
+
+
+        private static Player _player = new Player();
+
+        static void Main(string[] args)
+        {
+            GameEngine.Initialize();
+            _player.Name = "Anonymous";
+
+            
+            Console.ReadLine();
+        }
+    }
+}
