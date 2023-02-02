91fabb7e42ba0de4017be69db154cf0da051961d
MUKILTEO\1630410 <1630410@mukilteo.wednet.edu>
2023-02-02 09:03:16 -0800

    basic program and classes set up

--- CRPGProject/Player.cs
+++ CRPGProject/Player.cs
@@ -9,8 +9,61 @@ namespace CRPGProject
     public class Player
     {
         public string Name { set; get; }
+        public Location CurrentLocation { set; get; }
 
+        public void MoveTo(Location loc)
+        {
+            CurrentLocation = loc;
+        }
 
+        public void MoveNorth()
+        {
+            if(CurrentLocation.LocationToNorth != null)
+            {
+                MoveTo(CurrentLocation.LocationToNorth);
+            }
+            else
+            {
+                Console.WriteLine("there is impassable terrain to the North.");
+            }
+        }
 
+        public void MoveEast()
+        {
+            if (CurrentLocation.LocationToEast != null)
+            {
+                MoveTo(CurrentLocation.LocationToEast);
+            }
+            else
+            {
+                Console.WriteLine("there is impassable terrain to the East.");
+            }
+        }
+
+        public void MoveSouth()
+        {
+            if (CurrentLocation.LocationToSouth != null)
+            {
+                MoveTo(CurrentLocation.LocationToSouth);
+            }
+            else
+            {
+                Console.WriteLine("there is impassable terrain to the South.");
+            }
+        }
+
+        public void MoveWest()
+        {
+            if (CurrentLocation.LocationToWest != null)
+            {
+                MoveTo(CurrentLocation.LocationToWest);
+            }
+            else
+            {
+                Console.WriteLine("there is impassable terrain to the West.");
+            }
+        }
+
+        //class Player
     }
 }
--- CRPGProject/Program.cs
+++ CRPGProject/Program.cs
@@ -17,10 +17,73 @@ namespace CRPGProject
         {
             GameEngine.Initialize();
             _player.Name = "Anonymous";
+            _player.MoveTo(World.LocationByID(World.LOCATION_ID_HOME));
+
+           while (true)
+            {
+                Console.Write("> ");
+                string userInput = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(userInput))
+                {
+                    continue;
+                }
+                string cleanedInput = userInput.ToLower();
+
+                if(cleanedInput == "exit")
+                {
+                    break;
+                }
+                ParseInput(cleanedInput);
+
+
+            } //while
 
            
             
-            Console.ReadLine();
+        } //main
+
+
+        public static void ParseInput(string input)
+        {
+            if (input.Contains("help"))
+            {
+                Console.WriteLine("Help is coming soon...");
+
+            } else if (input.Contains("look")) {
+
+                DisplayCurrentLocation();
+
+            } else if (input.Contains("north")) {
+            
+                _player.MoveNorth();
+
+            } else if (input.Contains("east"))
+            {
+                _player.MoveEast();
+
+            } else if (input.Contains("south"))
+            {
+                _player.MoveSouth();
+
+            } else if (input.Contains("west"))
+            {
+                _player.MoveWest();
+            }
+            
+            else
+            {
+                Console.WriteLine("that isn't currently possible at the moment. please give a new command");
+            }
+        }//ParseInput
+
+        private static void DisplayCurrentLocation()
+        {
+            Console.WriteLine("You are at: {0}",_player.CurrentLocation.Name);
+            if(_player.CurrentLocation.Description != "")
+            {
+                Console.WriteLine("\t{0}\n", _player.CurrentLocation.Description);
+            }
+
         }
     }
 }
